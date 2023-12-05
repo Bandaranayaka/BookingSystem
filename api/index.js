fb5fd4410f66209f23e6cc502e1b6e0fd7ba8d69
Bandaranayaka <3lakshana1124@gmail.com>
2023-12-06 05:29:29 +0530

    added VerifyAdmin Job

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
 import cookieParser from "cookie-parser";
+
+
+
 const app = express()
 dotenv.config();
 
