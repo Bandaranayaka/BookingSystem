9f51b749ec198e3543d034d3b631d7ac42d59eb9
Bandaranayaka <3lakshana1124@gmail.com>
2023-12-06 01:30:17 +0530

    update register user

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,15 +1,22 @@
 import User from "../models/User.js"
+import bcrypt from "bcryptjs"
 
 export const register = async (req,res,next)=>{
     try{
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+
+
        const newUser = new User({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password,
-       })
+        password:hash,
+       });
 
        await newUser.save()
-       res.status(20).send("User has been created.")
+       res.status(200).send("User has been created.")
     }catch(err){
         next(err)
     }
