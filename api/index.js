6b6a3b55b04f9697e9224d816dd90030e7e49a80
Bandaranayaka <3lakshana1124@gmail.com>
2023-12-05 20:09:47 +0530

    Hotel create, update and delete

--- api/index.js
+++ api/index.js
@@ -23,10 +23,12 @@ mongoose.connection.on("disconnected", ()=> {
 
 
 //Middlewares
+app.use(express.json())
+
 app.use("/api/auth", authRoute);
-app.use("/api/users", authRoute);
-app.use("/api/hotels", authRoute);
-app.use("/api/rooms", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
 
 
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,8 +1,52 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
+//CREATE--------------------------------------
+router.post("/", async (req,res)=>{
+
+    const newHotel = new Hotel(req.body)
+
+    try{
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+    }catch(err){
+        res.status(500).json(err)
+    }    
+});
+
+//UPDATE--------------------------------------
+router.put("/:id", async (req,res)=>{
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id,
+             { $set: req.body},
+             {new : true }
+            );
+        res.status(200).json(updatedHotel)
+    }catch(err){
+        res.status(500).json(err)
+    }    
+});
+
+//DELETE--------------------------------------
+router.delete("/:id", async (req,res)=>{
+    try{
+        await Hotel.findByIdAndDelete(
+             req.params.id,
+             { $set: req.body},
+             {new : true }
+            );
+        res.status(200).json("Hotel has been deleted")
+    }catch(err){
+        res.status(500).json(err)
+    }    
+});
+
+//GET-----------------------------------------
+
+//GET ALL-------------------------------------
 
 
 
-export default router
+export default router;
