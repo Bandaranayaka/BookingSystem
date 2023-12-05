d0e534d36569663809ea18e7c9dc465ff26bcc05
Bandaranayaka <3lakshana1124@gmail.com>
2023-12-06 04:08:20 +0530

    added user authontiaction

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { updateUser, deleteUser, getUser, getUsers  } from "../controllers/user.js";
+import { 
+      updateUser,
+      deleteUser,
+      getUser, 
+      getUsers  } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("hello user, you are logged in ")
+}) 
 
 //UPDATE--------------------------------------
 router.put("/:id", updateUser); 
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -7,5 +7,9 @@ export const verifyToken = (req, res, next)=>{
         return next(createError(401, "You are not authenticated!"))
     }
 
-   jwt.verify(token,process.env.JWT, (err, user)) 
+   jwt.verify(token,process.env.JWT, (err, user) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+    req.user = user;
+    next()
+   });
 }
