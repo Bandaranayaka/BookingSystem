460ae22dab3a4c0b84337d29010e86109a724688
Bandaranayaka <3lakshana1124@gmail.com>
2023-12-06 03:59:40 +0530

    add verify token

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -36,7 +36,8 @@ export const login = async (req,res,next)=>{
     if(!isPasswordCorrect)
      return next(createError(400, "Wrong password or username!"));
 
-     const token = jwt.sign({id:user._id, isAdmin: user.isAdmin},
+     const token = jwt.sign(
+        {id:user._id, isAdmin: user.isAdmin},
          process.env.JWT
          );
        
