0a35ccb1b029684f7a7c8da54e6f58841c27c91b
Bandaranayaka <3lakshana1124@gmail.com>
2023-12-05 22:34:16 +0530

    add conrollers

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,21 +1,11 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
+// import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
 //CREATE--------------------------------------
-router.post("/", async (req,res)=>{
-
-    const newHotel = new Hotel(req.body)
-
-    try{
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    }catch(err){
-        res.status(500).json(err)
-    }    
-});
+router.post("/", createHotel);
 
 //UPDATE--------------------------------------
 router.put("/:id", async (req,res)=>{
