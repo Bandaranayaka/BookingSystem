49fe02b0665b59f2d96dc2baa774a8e672aef652
Bandaranayaka <3lakshana1124@gmail.com>
2023-12-06 08:31:01 +0530

    added Delete rooms

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -34,8 +34,16 @@ export const updateRoom = async (req, res, next)=> {
     }  
 };
 export const deleteRoom = async (req, res, next)=> {
+    const hotelId = req.params.hotelid;
     try{
-        await Room.findByIdAndDelete(req.params.id);           
+        await Room.findByIdAndDelete(req.params.id); 
+        try{
+            await Hotel.findByIdAndUpdate(hotelId,{
+                $pull: {rooms: req.params.id},
+            });
+        }catch(err){
+            next(err);
+        }          
         res.status(200).json("Room has been deleted");
     }catch(err){
         next(err);
